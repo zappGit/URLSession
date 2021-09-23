a7b859d1b8f1f3382b9495ace8e78506a7a6edc8
Artyom Khrebtov <t.khrebtov@yandex.ru>
2021-09-23 19:11:54 +0500

    Initial Commit

---
+++ URLSession/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  URLSession
+//
+//  Created by Артем Хребтов on 23.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
